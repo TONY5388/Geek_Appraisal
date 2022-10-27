92959934bd3516b699df7a0775a2fc3e0e320b47
TONY5388 <89394212+TONY5388@users.noreply.github.com>
2022-10-27 19:49:32 +0800

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	printf("Hello World! %f\n", (float)exp(1));
 	return 0;
 }
-
